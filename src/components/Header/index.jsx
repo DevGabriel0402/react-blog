b7bcc3401e9cdbd008c61a3e8efef80fc9af731c
DevGabriel0402 <gabriel.profissional01@gmail.com>
2025-01-03 14:15:07 -0300

    estilizando o menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,40 @@
+import logoSvg from "../../assets/svg/logo-blog.svg";
+import searchSvg from "../../assets/svg/search-icon.svg";
+
 export const Header = () => {
-  return <h2>Header</h2>;
+  return (
+    <>
+      <header className="flex-space-between">
+        <div className="logo">
+          <img src={logoSvg} alt="logo-blog" />
+        </div>
+        <div className="search">
+          <img src={searchSvg} alt="search-icon" className="search-icon" />
+          <input
+            type="text"
+            name="search"
+            className="input-search"
+            placeholder="Buscar artigos, podcasts, destaques..."
+          />
+        </div>
+        <ul className="menu">
+          <li>
+            <a href="#" className="nav-link">
+              Categories
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
 };
